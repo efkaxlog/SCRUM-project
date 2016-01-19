87519219f5da779081a0159256da3b3a8b0a654d
Joseph Gillen <josephgillen@Josephs-MacBook-Pro.local>
2016-01-19 14:55:47 +0000

    changes to dataHandling

--- src/DataHandling.java
+++ src/DataHandling.java
@@ -10,48 +10,26 @@ public class DataHandling
 {
 	
 	public ArduinoConnector arduino;
-	
-	//Heat flux sensor:
-		//Sensor ID, Sensor Name, Sensor type, time stamp, heat flux data, surface temp data, air temp data 
-	//External temperature sensor:
-	    //Sensor ID, Sensor Name, Sensor type, time stamp, surface temp data, air temp data
-    //Air temperature data:
-        //Sensor ID, Sensor Name, Sensor type, time stamp, air temp data
-	
-	
-	//String variables
-	int id;
-	String sensorName;
-	String sensorType;
-	//TimeStamp timeStamp; needs an import
-	float airTempData;
-	
-	//Heat flux sensor variables
-	float heatFluxTemp;
-	float internalWallSurfaceTemp;
-	float internalAirTemp;
-	
-	//External temperature sensor variables
-	float externalSurfaceTemp; 
-	float externalAirTemp;
-	
-	//Air temperature data variables
-	//N/A
-	
+	//Connection connection;
+	String dataString;
 
 	public DataHandling()
 	{
 		
 		
 		//Needs to be uncommented when classes are added.
-		//Connection connection = new Connection();
+		//connection = new Connection();
+		 
+	}
+	
+	public void handleObject()
+	{
 		//dataString = connection.getStream();
 		
-		//temp string to simulate data from arduino
-		String dataString = "10,Temp,Int Temp,27,89,29,65";
+		////temp string to simulate data from arduino
+		dataString = "10,Temp,Int Temp,27,89,29,65";
 		
-		parseString(dataString); 
-		
+		parseString(dataString);
 		
 		
 	}
