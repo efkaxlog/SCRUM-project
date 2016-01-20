b169e72078768e7a5d47577aaa9bf2bdbff2b7b8
Ahmed <a.m.khan3@edu.salford.ac.uk>
2016-01-20 11:42:07 +0000

    add classes

---
+++ src/Customer.java
@@ -0,0 +1,11 @@
+public class Customer {
+		String name;
+		int age;
+		double balance;
+	public Customer(String name, int age, double balance) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.balance = balance;
+	}
+}
