9243802c2df01588b969ff99a97552b705f8791d
Filip Kowalczyk <efkaxlog@gmail.com>
2016-01-22 14:34:51 +0000

    add some utilities methods

--- src/Utilities.java
+++ src/Utilities.java
@@ -1,4 +1,9 @@
 
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 public abstract class Utilities {
 
 	/**
@@ -8,4 +13,23 @@ public abstract class Utilities {
 		return str.substring(0, str.length()-1);
 	}
 	
+	public static Timestamp getCurrentTimestamp() {
+		Calendar c = Calendar.getInstance();
+		java.util.Date now = c.getTime();
+		return new Timestamp(now.getTime());
+	}
+	
+	public static Timestamp getTimestampFromString(String timestampString) {
+		String pattern = "yyyy-MM-dd hh:mm:ss.SSS";
+		Timestamp timestamp = null;
+		try {
+			SimpleDateFormat dateFormat = new SimpleDateFormat(pattern);
+			Date parsedDate = dateFormat.parse(timestampString);
+			timestamp = new Timestamp(parsedDate.getTime());
+		} catch (Exception e) {
+			System.out.println("Failed converting string to timestamp");
+			e.printStackTrace();
+		}
+		return timestamp;
+	}
 }
