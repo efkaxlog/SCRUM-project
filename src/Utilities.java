ce0d6388643d25837196930b90a6a6e2e304110e
Filip Kowalczyk <efkaxlog@gmail.com>
2016-01-22 12:27:18 +0000

    add Utilities class

---
+++ src/Utilities.java
@@ -0,0 +1,11 @@
+
+public abstract class Utilities {
+
+	/**
+	 * returns string without it's last character
+	 */
+	public static String removeLastChar(String str) {
+		return str.substring(0, str.length()-1);
+	}
+	
+}
