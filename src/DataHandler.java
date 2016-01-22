3c3e9df940ffcd5867c3ecd721414d6f6d62df0c
Filip Kowalczyk <efkaxlog@gmail.com>
2016-01-22 14:36:38 +0000

    add boolean var and fix order in which variables are assigned using scanner parser

--- src/DataHandler.java
+++ src/DataHandler.java
@@ -1,7 +1,6 @@
 
 //import ArduinoConnection.Connection;
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.Scanner;
 import sensors.*;
 
@@ -11,34 +10,35 @@ import java.sql.Timestamp;
 public class DataHandler {
 	// Connection connection;
 	Database database;
-	ArrayList<Sensor> allSensors = new ArrayList<Sensor>();
-
 	
 	public DataHandler() {
 		database = new Database();
 	}
 	
 	//fetches the data from the database class
-	public ArrayList<Sensor> fetchUIdata() {		
-		ArrayList<String> sensorDataStrings = null;
+	public ArrayList<Sensor> makeSensorsFromDB(String tableName) {	
+		boolean isDataFromArduino = false;
+		ArrayList<String> sensorDataStrings = new ArrayList<String>();
 		try {
-			sensorDataStrings = new ArrayList<String>(database.getAllSensorsStrings());
+			database.addSensorStrings(tableName, sensorDataStrings);
 		} catch (SQLException e) {
-			System.out.println("Failed to return sensors data from database");
+			System.out.println("Failed to retrieve sensors data "
+					+ "strings from database. (table name: " +tableName);
 			e.printStackTrace();
 		}
 		
+		ArrayList<Sensor> sensors = new ArrayList<Sensor>();
 		if (sensorDataStrings != null) {
 			for(String row : sensorDataStrings) {
-				allSensors.add(makeSensor(row));
+				sensors.add(makeSensor(row, isDataFromArduino));
 			}
 		}
-		return allSensors;
+		return sensors;
 	}
 
-	public void handleObject(String dataString) {
+	public void handleObject(String dataString, boolean isDataFromArduino) {
 		try {
-			database.insertSensorIntoTable(makeSensor(dataString));
+			database.insertSensorIntoTable(makeSensor(dataString, isDataFromArduino));
 		} catch (SQLException e) {
 			System.out.println("Failed to insert sensor into table.");
 			System.out.println("Data string: " + dataString);
@@ -46,36 +46,41 @@ public class DataHandler {
 		}
 	}
 
-	public Sensor makeSensor(String dataString) {
+	public Sensor makeSensor(String dataString, boolean isDataFromArduino) {
 		Sensor sensor;
 		Scanner scanner = new Scanner(dataString);
 		scanner.useDelimiter(",");
 		int id = scanner.nextInt();
 		String sensorType = scanner.next();
 		String sensorName = scanner.next();
-		float airTempData = scanner.nextFloat();
-		Timestamp timestamp = getCurrentTimestamp();
-
+		
 		if (sensorName.equals("heat_flux1")) {
 			float heatFluxTemp = scanner.nextFloat();
 			float internalWallSurfaceTemp = scanner.nextFloat();
-			sensor = new HeatFluxSensor(id, sensorName, sensorType, timestamp, 
+			float airTempData = scanner.nextFloat();
+			
+			sensor = new HeatFluxSensor(id, sensorName, sensorType,
 					heatFluxTemp, internalWallSurfaceTemp, airTempData);
 			
 		} else if (sensorName.equals("Ext Temp")) {
 			float externalSurfaceTemp = scanner.nextFloat();
+			float airTempData = scanner.nextFloat();
 			sensor = new ExternalTempSensor(id, sensorName, sensorType,
-					timestamp, externalSurfaceTemp, airTempData);
+					externalSurfaceTemp, airTempData);
 			
 		} else {
-			sensor = new AirTempSensor(id, sensorName, sensorType, timestamp, airTempData);
+			float airTempData = scanner.nextFloat();
+			sensor = new AirTempSensor(id, sensorName, sensorType, airTempData);
 		}
+		Timestamp ts;
+		if (isDataFromArduino) {
+			ts = Utilities.getCurrentTimestamp();
+		} else {
+			ts = Utilities.getTimestampFromString(scanner.next());
+		}
+		sensor.setTimestamp(ts);
 		return sensor;
 	}
 	
-	private Timestamp getCurrentTimestamp() {
-		Calendar c = Calendar.getInstance();
-		java.util.Date now = c.getTime();
-		return new Timestamp(now.getTime());
-	}
+	
 }
